db5caec96e5775f2862bf7f40a37a71b0256e77e
pranshugarg <pranshugarg02@gmail.com>
2019-03-16 20:06:27 +0530

    Greet new users and alert others

--- server/server.js
+++ server/server.js
@@ -11,17 +11,29 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+
 io.on('connection', (socket) => {
   console.log('New user connected');
 
   socket.emit('newMessage', {
-    from: 'John',
-    text: 'See you then',
-    createdAt: 123123
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
   });
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+     io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+     });
   });
 
   socket.on('disconnect', () => {
@@ -32,3 +44,4 @@ io.on('connection', (socket) => {
 server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
+
