fe0bc4902a30f996894e46bc42e6b648feb71cab
pranshugarg <pranshugarg02@gmail.com>
2019-03-16 20:39:20 +0530

    Event acknowledgements through callback

--- public/js/index.js
+++ public/js/index.js
@@ -16,3 +16,8 @@ socket.on('disconnect', function () {
 socket.on('newMessage', function (message) {
   console.log('newMessage', message);
 });
+
+socket.emit('createMessage', {from: 'Frank', text: 'Hi'},
+   function (data) {
+  console.log('Got it', data);
+});
